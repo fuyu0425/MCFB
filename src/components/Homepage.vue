679c1df5372a40bb7da9163cfcc5317107726ae8
傅裕夫 <a0919610611@gmail.com>
2016-08-20 23:06:48 +0800

    test

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -20,31 +20,6 @@
   <script>
 
 
-    function IWantToLogout() {
-      FB.logout(function(response) {
-        $("#logout").hide();
-        var profile = "<div class=\"row\" id=\"login\"><div class=\"col s12 m10 offset-m1 l8 offset-l2\"><div class=\"card blue-grey darken-1\"><div class=\"card-image\"><img src="http://2.bp.blogspot.com/-7ywyKE1iKaA/Tyfbcob7R3I/AAAAAAAAIwQ/h83RQebeEJ8/s1600/2012-01-29_22.15.39.png"><span class=\"card-title\">Try it!</span></div><div class=\"card-content white-text\"><p>想體驗一下嗎?123</p></div><div class=\"card-action\"><a class=\"waves-effect waves-light blue darken-2 btn\" onclick=\"checkLoginState();\"><i class=\"fa fa-facebook-official\"></i>&nbsp;&nbsp;facebook login</a></div></div></div></div>";
-        $("main")[0].innerHTML = profile;
-        $("ul li:nth-child(3)")[0].innerHTML = "<li><a href=\"#\">Login</a></li>";
-      });
-    }
-
-
-
-
-    /*
-     (function(d, s, id) {
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) return;
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-     }(document, 'script', 'facebook-jssdk'));
-     */
-
-
-
-
   </script>
 </template>
 
@@ -101,6 +76,15 @@
               statusChangeCallback(response);
             }, {scope: 'email,public_profile,user_posts', return_scopes: true});
           }
+        });
+      }
+
+      function IWantToLogout() {
+        FB.logout(function(response) {
+          $("#logout").hide();
+         var  profile = "<div class=\"row\" id=\"login\"><div class=\"col s12 m10 offset-m1 l8 offset-l2\"><div class=\"card blue-grey darken-1\"><div class=\"card-image\"><img src=\"http://2.bp.blogspot.com/-7ywyKE1iKaA/Tyfbcob7R3I/AAAAAAAAIwQ/h83RQebeEJ8/s1600/2012-01-29_22.15.39.png\"><span class=\"card-title\">Try it!</span></div><div class=\"card-content white-text\"><p>想體驗一下嗎?123</p></div><div class=\"card-action\"><a class=\"waves-effect waves-light blue darken-2 btn\" onclick=\"checkLoginState();\"><i class=\"fa fa-facebook-official\"></i>&nbsp;&nbsp;facebook login</a></div></div></div></div>";
+          $("main")[0].innerHTML = profile;
+          $("ul li:nth-child(3)")[0].innerHTML = "<li><a href=\"#\">Login</a></li>";
         });
       }
       window.fbAsyncInit = function () {
