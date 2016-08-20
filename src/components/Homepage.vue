5fb959de74a065de9b1717488c6028613abea3cc
傅裕夫 <a0919610611@gmail.com>
2016-08-21 00:16:59 +0800

    test

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -49,7 +49,7 @@
       }(document, 'script', 'facebook-jssdk'));
     },
     methods: {
-      IWantToLogout(){
+      IWantToLogout :function (){
         FB.logout(function (response) {
           $("#logout").hide();
           var profile = "<div class=\"row\" id=\"login\"><div class=\"col s12 m10 offset-m1 l8 offset-l2\"><div class=\"card blue-grey darken-1\"><div class=\"card-image\"><img src=\"http://2.bp.blogspot.com/-7ywyKE1iKaA/Tyfbcob7R3I/AAAAAAAAIwQ/h83RQebeEJ8/s1600/2012-01-29_22.15.39.png\"><span class=\"card-title\">Try it!</span></div><div class=\"card-content white-text\"><p>想體驗一下嗎?</p></div><div class=\"card-action\"><a class=\"waves-effect waves-light blue darken-2 btn\" onclick=\"checkLoginState();\"><i class=\"fa fa-facebook-official\"></i>&nbsp;&nbsp;facebook login</a></div></div></div></div>";
@@ -57,7 +57,7 @@
           $("ul li:nth-child(3)")[0].innerHTML = "<li><a href=\"#\">Login</a></li>";
         });
       },
-      statusChangeCallback(response) {
+      statusChangeCallback : function(response) {
         if (response.status === 'connected') {
           $("#login").hide();
           var profile = "<div class=\"row\" id=\"logout\"><div class=\"col s12 m10 offset-m1 l8 offset-l2\"><div class=\"card blue-grey darken-1\"><div class=\"card-content white-text\"><span class=\"card-title\" id=\"UserName\"></span><p>Profile Information goes here</p></div><div class=\"card-action\"><a class=\"waves-effect waves-light blue darken-2 btn\" onclick=\"IWantToLogout();\"><i class=\"fa fa-facebook-official\"></i>&nbsp;&nbsp;facebook logout</a></div></div></div></div>";
@@ -75,7 +75,7 @@
            */
         }
       },
-      getPost()
+      getPost: function()
       {
         FB.api('/me/posts', function (response) {
           console.log(response.data);
@@ -95,7 +95,7 @@
         });
       },
 
-      checkLoginState() {
+      checkLoginState: function () {
         alert('hi')
         FB.getLoginStatus(function (response) {
           if (response.status === 'connected') {
