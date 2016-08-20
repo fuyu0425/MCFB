dae27f873ddfbb6464814c9d717bef2d1bc4975a
傅裕夫 <a0919610611@gmail.com>
2016-08-20 22:54:08 +0800

    test

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -17,74 +17,91 @@
       </div>
     </div>
   </main>
+  <script>
+    function statusChangeCallback(response) {
+      if (response.status === 'connected') {
+        $("#login").hide();
+        var profile = "<div class=\"row\" id=\"logout\"><div class=\"col s12 m10 offset-m1 l8 offset-l2\"><div class=\"card blue-grey darken-1\"><div class=\"card-content white-text\"><span class=\"card-title\" id=\"UserName\"></span><p>Profile Information goes here</p></div><div class=\"card-action\"><a class=\"waves-effect waves-light blue darken-2 btn\" onclick=\"IWantToLogout();\"><i class=\"fa fa-facebook-official\"></i>&nbsp;&nbsp;facebook logout</a></div></div></div></div>";
+        var key = response.authResponse.accessToken;
+        $("main")[0].innerHTML = profile;
+        FB.api('/me', function(response) {
+          $('#UserName')[0].innerHTML = response.name;
+          $("ul li:nth-child(3)")[0].innerHTML = "<li><a href=\"#\">"+response.name+"</a></li>";
+        });
+        var posts = getPost();
+        /*
+         $.post(receive.go, {
+         name: UserName;
+         text: posts;
+         });
+         */
+      }
+    }
+    function getPost()
+    {
+      FB.api('/me/posts',function (response) {
+        console.log(response.data);
+        var data=response.data
+        var ans=[];
+        for (var i=0 ;i < data.length;i++){
+          var tmp={};
+          var str="";
+          if('story' in data[i])str+=data[i].story;
+          if ('message' in data[i])str+=data[i].message;
+          tmp.message=str;
+          tmp.created_time=data[i].created_time;
+          ans.push(tmp);
+        }
+        console.log(ans);
+        return ans;
+      });
+    }
+    function checkLoginState() {
+      alert('hi')
+      FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+          statusChangeCallback(response);
+        }
+        else {
+          FB.login(function(response) {
+            cosole.log(response)
+            statusChangeCallback(response);
+          }, {scope: 'email,public_profile,user_posts', return_scopes: true});
+        }
+      });
+    }
+
+    function IWantToLogout() {
+      FB.logout(function(response) {
+        $("#logout").hide();
+        var profile = "<div class=\"row\" id=\"login\"><div class=\"col s12 m10 offset-m1 l8 offset-l2\"><div class=\"card blue-grey darken-1\"><div class=\"card-image\"><img src=\"http://2.bp.blogspot.com/-7ywyKE1iKaA/Tyfbcob7R3I/AAAAAAAAIwQ/h83RQebeEJ8/s1600/2012-01-29_22.15.39.png\"><span class=\"card-title\">Try it!</span></div><div class=\"card-content white-text\"><p>想體驗一下嗎?123</p></div><div class=\"card-action\"><a class=\"waves-effect waves-light blue darken-2 btn\" onclick=\"checkLoginState();\"><i class=\"fa fa-facebook-official\"></i>&nbsp;&nbsp;facebook login</a></div></div></div></div>";
+        $("main")[0].innerHTML = profile;
+        $("ul li:nth-child(3)")[0].innerHTML = "<li><a href=\"#\">Login</a></li>";
+      });
+    }
+
+
+
+
+    /*
+     (function(d, s, id) {
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) return;
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+     */
+
+
+
+    }
+  </script>
 </template>
 
 <script>
   export default {
     ready : function() {
-      function statusChangeCallback(response) {
-        if (response.status === 'connected') {
-          $("#login").hide();
-          var profile = "<div class=\"row\" id=\"logout\"><div class=\"col s12 m10 offset-m1 l8 offset-l2\"><div class=\"card blue-grey darken-1\"><div class=\"card-content white-text\"><span class=\"card-title\" id=\"UserName\"></span><p>Profile Information goes here</p></div><div class=\"card-action\"><a class=\"waves-effect waves-light blue darken-2 btn\" onclick=\"IWantToLogout();\"><i class=\"fa fa-facebook-official\"></i>&nbsp;&nbsp;facebook logout</a></div></div></div></div>";
-          var key = response.authResponse.accessToken;
-          $("main")[0].innerHTML = profile;
-          FB.api('/me', function(response) {
-            $('#UserName')[0].innerHTML = response.name;
-            $("ul li:nth-child(3)")[0].innerHTML = "<li><a href=\"#\">"+response.name+"</a></li>";
-          });
-          posts = getPost();
-          /*
-           $.post(receive.go, {
-           name: UserName;
-           text: posts;
-           });
-           */
-        }
-      }
-      function getPost()
-      {
-        FB.api('/me/posts',function (response) {
-          console.log(response.data);
-          var data=response.data
-          var ans=[];
-          for (var i=0 ;i < data.length;i++){
-            var tmp={};
-            var str="";
-            if('story' in data[i])str+=data[i].story;
-            if ('message' in data[i])str+=data[i].message;
-            tmp.message=str;
-            tmp.created_time=data[i].created_time;
-            ans.push(tmp);
-          }
-          console.log(ans);
-          return ans;
-        });
-      }
-      function checkLoginState() {
-        alert('hi')
-        FB.getLoginStatus(function(response) {
-          if (response.status === 'connected') {
-            statusChangeCallback(response);
-          }
-          else {
-            FB.login(function(response) {
-			cosole.log(response)
-              statusChangeCallback(response);
-            }, {scope: 'email,public_profile,user_posts', return_scopes: true});
-          }
-        });
-      }
-
-      function IWantToLogout() {
-        FB.logout(function(response) {
-          $("#logout").hide();
-          var profile = "<div class=\"row\" id=\"login\"><div class=\"col s12 m10 offset-m1 l8 offset-l2\"><div class=\"card blue-grey darken-1\"><div class=\"card-image\"><img src=\"http://2.bp.blogspot.com/-7ywyKE1iKaA/Tyfbcob7R3I/AAAAAAAAIwQ/h83RQebeEJ8/s1600/2012-01-29_22.15.39.png\"><span class=\"card-title\">Try it!</span></div><div class=\"card-content white-text\"><p>想體驗一下嗎?123</p></div><div class=\"card-action\"><a class=\"waves-effect waves-light blue darken-2 btn\" onclick=\"checkLoginState();\"><i class=\"fa fa-facebook-official\"></i>&nbsp;&nbsp;facebook login</a></div></div></div></div>";
-          $("main")[0].innerHTML = profile;
-          $("ul li:nth-child(3)")[0].innerHTML = "<li><a href=\"#\">Login</a></li>";
-        });
-      }
-
-
       window.fbAsyncInit = function() {
         FB.init({
           appId      : '1014922658562861',
@@ -97,17 +114,6 @@
           statusChangeCallback(response);
         });
       };
-
-      /*
-       (function(d, s, id) {
-       var js, fjs = d.getElementsByTagName(s)[0];
-       if (d.getElementById(id)) return;
-       js = d.createElement(s); js.id = id;
-       js.src = "https://connect.facebook.net/en_US/sdk.js";
-       fjs.parentNode.insertBefore(js, fjs);
-       }(document, 'script', 'facebook-jssdk'));
-       */
-
       (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
@@ -115,7 +121,5 @@
         js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.7&appId=1014922658562861";
         fjs.parentNode.insertBefore(js, fjs);
       }(document, 'script', 'facebook-jssdk'));
-
-    }
     }
 </script>
