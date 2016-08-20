90a87dfc0f9417289d5d6f9eedee0be9c8437ea6
傅裕夫 <a0919610611@gmail.com>
2016-08-21 01:22:37 +0800

    test

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -34,7 +34,17 @@
   </main>
 
 </template>
+<style>
+  body {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
 
+  main {
+    flex: 1 0 auto;
+  }
+</style>
 <script>
 
 
