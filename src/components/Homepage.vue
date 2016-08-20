2537f244a497b4a018a89a1b7147edbe1c67c3c6
傅裕夫 <a0919610611@gmail.com>
2016-08-21 00:58:47 +0800

    test

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -3,13 +3,25 @@
     <div class="row" >
       <div class="col s12 m10 offset-m1 l8 offset-l2">
         <div class="card blue-grey darken-1">
+          <div v-if="statuss ==1">
           <div class="card-image">
             <img src="http://2.bp.blogspot.com/-7ywyKE1iKaA/Tyfbcob7R3I/AAAAAAAAIwQ/h83RQebeEJ8/s1600/2012-01-29_22.15.39.png">
             <span class="card-title">Try it!</span>
           </div>
+
           <div class="card-content white-text">
             <p>想體驗一下嗎?</p>
           </div>
+            </div>
+          <div v-else>
+            <div class="row">
+              <div class="col s12 m10 offset-m1 l8 offset-l2">
+                <div class="card blue-grey darken-1">
+                  <div class="card-content white-text">
+                    <span class="card-title\" id="{{ username }}"></span>
+                    <p>Profile Information goes here</p>
+                  </div>
+            </div>
           <div class="card-action">
             <div v-if="statuss == 1">
             <a class="waves-effect waves-light blue darken-2 btn" v-on:click="checkLoginState"><i class="fa fa-facebook-official"></i>&nbsp;&nbsp;facebook login</a>
@@ -31,7 +43,8 @@
     data ()
     {
       return {
-      statuss : 1
+      statuss : 1 ,
+        username : ''
     }
   },
   ready: function () {
@@ -66,7 +79,7 @@
         });
       },
       statusChangeCallback : function(response) {
-        alert('status')
+        //alert('status')
         var self = this
         console.log(self.statuss)
         if (response.status === 'connected') {
@@ -75,6 +88,7 @@
           FB.api('/me', function (response) {
             //$('#UserName')[0].innerHTML = response.name;
             alert(response.name);
+            self.username=response.name
             $("ul li:nth-child(3)")[0].innerHTML = "<li><a href=\"#\">" + response.name + "</a></li>";
           });
           var posts = this.getPost();
@@ -89,7 +103,7 @@
       getPost: function()
       {
         FB.api('/me/posts', function (response) {
-          console.log(response.data);
+          //console.log(response.data);
           var data = response.data
           var ans = [];
           for (var i = 0; i < data.length; i++) {
@@ -101,21 +115,21 @@
             tmp.created_time = data[i].created_time;
             ans.push(tmp);
           }
-          console.log(ans);
+         // console.log(ans);
           return ans;
         });
       },
 
       checkLoginState: function () {
         var self = this
-        alert('hi')
+        //alert('hi')
         FB.getLoginStatus(function (response) {
           if (response.status === 'connected') {
             this.statusChangeCallback(response);
           }
           else {
             FB.login(function (response) {
-              console.log(response)
+            //  console.log(response)
               self.statusChangeCallback(response);
             }, {scope: 'email,public_profile,user_posts', return_scopes: true});
           }
