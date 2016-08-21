fe02ba33bd9aab80d7a7780796c4b793baf222a7
傅裕夫 <a0919610611@gmail.com>
2016-08-21 08:34:38 +0800

    test

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -85,6 +85,7 @@
           FB.api('/me', function (response) {
             self.username=response.name
             $("ul li:nth-child(3)")[0].innerHTML = "<li><a href=\"#\">" + response.name + "</a></li>";
+            self.getPost();
           });
 
           /*
